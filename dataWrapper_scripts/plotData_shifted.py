6bc9f7ad8f8be3fac837524a2e304b1b272c387d
Christoph Smoch <christoph.smoch@uni-bonn.de>
2023-11-01 14:01:28 -0400

    plotshift

--- dataWrapper_scripts/plotData_shifted.py
+++ dataWrapper_scripts/plotData_shifted.py
@@ -21,20 +21,20 @@ def load_data_from_json(file_path):
         data_list = json.load(file)
     return data_list
 
-def accumulate_data_polyfem_base(data_list, x_key, y_key):
+def accumulate_data_polyfem(data_list, x_key, y_key, strH, H):
     x_values_all = []
     y_values_all = []
     names_all = []
 
     for entry in data_list:
-        
-        x_values = entry[x_key]
-        y_values = entry[y_key]
-        name = entry.get('name', [])
+        if entry[strH] == H:
+            x_values = entry[x_key]
+            y_values = entry[y_key]
+            name = entry.get('name', [])
 
-        x_values_all.append(x_values)
-        y_values_all.append(y_values)
-        names_all.append(name)
+            x_values_all.append(x_values)
+            y_values_all.append(y_values)
+            names_all.append(name)
 
     return x_values_all, y_values_all, names_all                
 
@@ -49,15 +49,17 @@ if __name__ == "__main__":
     data_list1 = load_data_from_json(file_path1)
     data_list2 = load_data_from_json(file_path2)
 
-    
-    x_values1, y_values1, name1 = accumulate_data_polyfem_base(data_list1, x_key, y_key)
-    x_values2, y_values2, name2 = accumulate_data_polyfem_base(data_list2, x_key, y_key)
+    inplaneRes = [2.**(-i) for i in range(5,8)]
+    if x_key == "h":
+        for H in inplaneRes:
+            x_values1, y_values1, name1 = accumulate_data_polyfem(data_list1, x_key, y_key, "H", H)
+            x_values2, y_values2, name2 = accumulate_data_polyfem(data_list2, x_key, y_key, "H", H)
 
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x_values1, y=y_values1, mode= 'lines+markers',
-                                    name="shift" , text="shift", marker=dict(size=10)))
-    fig.add_trace(go.Scatter(x=x_values2, y=y_values2, mode= 'lines+markers',
-                                    name="reg" , text="reg", marker=dict(size=10)))
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=x_values1, y=y_values1, mode= 'lines+markers',
+                                            name="shift_H=2^" + str(np.log2(H)) , text="shift_H=2^" + str(np.log2(H)) , marker=dict(size=10)))
+            fig.add_trace(go.Scatter(x=x_values2, y=y_values2, mode= 'lines+markers',
+                                            name="reg_H=2^" + str(np.log2(H))  , text="reg_H=2^" + str(np.log2(H)) , marker=dict(size=10)))
 
     
     
